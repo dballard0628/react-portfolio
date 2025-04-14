14b07b119969764f5078bce766f1f530cee247db
dballard0628 <ballarddanag@gmail.com>
2025-04-14 15:13:56 -0400

    fixed errors to be able to deploy portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home'; 
